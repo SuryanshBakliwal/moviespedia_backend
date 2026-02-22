c44e06d042d1652121b4c72ee3e7a2745ae5dde3
Suryansh Bakliwal <sbakliwal9@gmail.com>
2026-02-22 17:34:37 +0530

    fixed issue regarding sending mail

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,24 +1,45 @@
 // Utils/mailer.js
 import nodemailer from "nodemailer";
+import { Resend } from "resend";
+// const transporter = nodemailer.createTransport({
+//     host: process.env.HOST,
+//     port: process.env.MAIL_PORT = 465,
+//     secure: true,
+//     auth: {
+//         user: process.env.DB_AUTH_MAIL,
+//         pass: process.env.DB_AUTH_MAIL_PASSWORD,
+//     },
+// });
 
-const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    port: process.env.MAIL_PORT = 465,
-    secure: true,
-    auth: {
-        user: process.env.DB_AUTH_MAIL,
-        pass: process.env.DB_AUTH_MAIL_PASSWORD,
-    },
-});
+// export const sendMail = async (to, subject, html) => {
+//     try {
+//         await transporter.sendMail({
+//             from: `"Moviespedia" <${process.env.DB_AUTH_MAIL}>`,
+//             to,
+//             subject,
+//             html,
+//         });
+//         console.log(`Email sent to ${to}`);
+//     } catch (err) {
+//         console.error("Error sending email:", err);
+//         throw new Error("Email sending failed");
+//     }
+// };
+
+
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async (to, subject, html) => {
     try {
-        await transporter.sendMail({
-            from: `"Moviespedia" <${process.env.DB_AUTH_MAIL}>`,
+        await resend.emails.send({
+            from: "Moviespedia <onboarding@resend.dev>",
             to,
             subject,
             html,
         });
+
         console.log(`Email sent to ${to}`);
     } catch (err) {
         console.error("Error sending email:", err);
