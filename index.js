41db492a4f8e914ad6e276eddd312cfa319d6663
Suryansh Bakliwal <sbakliwal9@gmail.com>
2026-02-22 18:10:48 +0530

    Fixes

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const app = express();
 Connection();
 
 // // for local
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
-
-// for Live
 // app.use(cors({
-//   origin: "https://movieespedia.netlify.app",
+//   origin: "http://localhost:3000",
 //   credentials: true
 // }));
+
+// for Live
+app.use(cors({
+  origin: "https://movieespedia.netlify.app",
+  credentials: true
+}));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
